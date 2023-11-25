8f53a398662b652df783de6db425e87be585a5ab
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2023-11-25 11:59:31 -0300

    first commit

---
+++ saleservice/src/test/java/br/com/mmmsieto/saleservice/SaleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mmmsieto.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
