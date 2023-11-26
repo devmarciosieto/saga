cdf8261d460c57770b66dd84cacbd7b7d944fb64
marcio sieto <mmmsieto@hotmail.com>
2023-11-26 11:35:08 -0300

    add config

--- saleservice/src/main/java/br/com/mmmsieto/saleservice/adapters/out/SaveSaleAdapter.java
+++ saleservice/src/main/java/br/com/mmmsieto/saleservice/adapters/out/SaveSaleAdapter.java
@@ -1,4 +1,24 @@
 package br.com.mmmsieto.saleservice.adapters.out;
 
-public class SaveSaleAdapter {
+import br.com.mmmsieto.saleservice.adapters.out.repository.SaleRepository;
+import br.com.mmmsieto.saleservice.adapters.out.repository.convert.SaleConvert;
+import br.com.mmmsieto.saleservice.adapters.out.repository.convert.SaleEntityConvert;
+import br.com.mmmsieto.saleservice.application.core.domain.Sale;
+import br.com.mmmsieto.saleservice.application.ports.out.SaveSaleOutputPort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveSaleAdapter implements SaveSaleOutputPort {
+
+    private final SaleRepository saleRepository;
+
+    public SaveSaleAdapter(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
+    @Override
+    public Sale save(Sale sale) {
+        return SaleConvert.toSale(saleRepository.save(SaleEntityConvert.toEntity(sale)));
+    }
+
 }
--- saleservice/src/main/java/br/com/mmmsieto/saleservice/config/usecase/CreateSaleConfig.java
+++ saleservice/src/main/java/br/com/mmmsieto/saleservice/config/usecase/CreateSaleConfig.java
@@ -1,16 +1,19 @@
-//package br.com.mmmsieto.saleservice.config.usecase;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class CreateSaleConfig {
-//
-//    @Bean
-//    public CreateSaleUseCase createSaleUseCase(
-//            SaveSaleAdapter saveSaleAdapter,
-//            SendCreatedSaleAdapter sendCreatedSaleAdapter) {
-//        return new CreateSaleUseCase(saveSaleAdapter, sendCreatedSaleAdapter);
-//    }
-//
-//}
+package br.com.mmmsieto.saleservice.config.usecase;
+
+import br.com.mmmsieto.saleservice.adapters.out.SaveSaleAdapter;
+import br.com.mmmsieto.saleservice.adapters.out.SendCreatedSaleAdapter;
+import br.com.mmmsieto.saleservice.application.core.usecase.CreateSaleUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CreateSaleConfig {
+
+    @Bean
+    public CreateSaleUseCase createSaleUseCase(
+            SaveSaleAdapter saveSaleAdapter,
+            SendCreatedSaleAdapter sendCreatedSaleAdapter) {
+        return new CreateSaleUseCase(saveSaleAdapter, sendCreatedSaleAdapter);
+    }
+
+}
