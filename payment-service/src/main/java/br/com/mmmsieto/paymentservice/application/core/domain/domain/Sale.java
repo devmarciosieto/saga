c856573562c18ce4868844ee89029a9bb849816f
marcio sieto <mmmsieto@hotmail.com>
2023-12-01 00:28:16 -0300

    add domain

---
+++ payment-service/src/main/java/br/com/mmmsieto/paymentservice/application/core/domain/domain/Sale.java
@@ -0,0 +1,82 @@
+package br.com.mmmsieto.paymentservice.application.core.domain.domain;
+
+
+
+import br.com.mmmsieto.paymentservice.application.core.domain.domain.enums.SaleStatus;
+
+import java.math.BigDecimal;
+
+public class Sale {
+
+    private String id;
+
+    private Long productId;
+
+    private Long userId;
+
+    private BigDecimal value;
+
+    private SaleStatus status;
+
+    private Integer quantity;
+
+    public Sale() {
+    }
+
+    public Sale(String id, Long productId, Long userId, BigDecimal value, SaleStatus status, Integer quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.userId = userId;
+        this.value = value;
+        this.status = status;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public SaleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SaleStatus status) {
+        this.status = status;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ payment-service/src/main/java/br/com/mmmsieto/paymentservice/application/core/domain/domain/enums/SaleEvent.java
@@ -0,0 +1,11 @@
+package br.com.mmmsieto.paymentservice.application.core.domain.domain.enums;
+
+public enum SaleEvent {
+
+    CREATED_SALE,
+    UPDATED_INVENTORY,
+    VALIDATED_PAYMENT,
+    ROLLBACK_INVENTORY,
+    FAILED_PAYMENT
+
+}
