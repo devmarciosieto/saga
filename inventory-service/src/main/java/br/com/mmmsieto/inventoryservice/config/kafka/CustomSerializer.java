49bf982f9392246cbf93f5cb4f839d0684f2dcad
marcio sieto <mmmsieto@hotmail.com>
2023-12-03 16:45:03 -0300

    fix config kafka

--- inventory-service/src/main/java/br/com/mmmsieto/inventoryservice/config/kafka/CustomSerializer.java
+++ inventory-service/src/main/java/br/com/mmmsieto/inventoryservice/config/kafka/CustomSerializer.java
@@ -3,24 +3,24 @@ package br.com.mmmsieto.inventoryservice.config.kafka;
 
 import br.com.mmmsieto.inventoryservice.adapters.out.message.SaleMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
-import org.hibernate.type.SerializationException;
 
 public class CustomSerializer implements Serializer<SaleMessage> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public byte[] serialize(String s, SaleMessage saleMessage)  {
+    public byte[] serialize(String s, SaleMessage saleMessage) {
         try {
-            if (saleMessage == null) {
+            if (saleMessage == null){
                 return null;
             }
             return objectMapper.writeValueAsBytes(saleMessage);
-
         } catch (Exception e) {
-            throw new SerializationException("Error serializing value", e);
+            throw new SerializationException("Error when serializing SaleMessage to byte[]");
         }
     }
 
 }
+
