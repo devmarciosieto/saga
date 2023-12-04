f050d451a6bb4e38232a255596689b8db0ae731b
marcio sieto <mmmsieto@hotmail.com>
2023-12-04 17:30:52 -0300

    config kafka

--- inventory-service/src/main/java/br/com/mmmsieto/inventoryservice/adapters/in/consumer/ReceiveSaleToCreditInventoryConsumer.java
+++ inventory-service/src/main/java/br/com/mmmsieto/inventoryservice/adapters/in/consumer/ReceiveSaleToCreditInventoryConsumer.java
@@ -4,6 +4,7 @@ import br.com.mmmsieto.inventoryservice.adapters.out.message.SaleMessage;
 import br.com.mmmsieto.inventoryservice.application.core.domain.enums.SaleEvent;
 import br.com.mmmsieto.inventoryservice.application.ports.in.CreditInventoryInputPort;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -18,12 +19,12 @@ public class ReceiveSaleToCreditInventoryConsumer {
     }
 
     @KafkaListener(topics = "tp-saga-sale", groupId = "inventory-credit")
-    public void receive(SaleMessage saleMessage) {
+    public void receive(ConsumerRecord<String, SaleMessage> saleMessage) {
         log.info("Receive sale to credit inventory: {}", saleMessage);
 
-        if (SaleEvent.FAILED_PAYMENT.equals(saleMessage.getEvent())) {
+        if (SaleEvent.FAILED_PAYMENT.equals(saleMessage.value().getEvent())) {
             log.info("Start Sale failed payment, compensating transaction: {}", saleMessage);
-            creditInventoryInputPort.credit(saleMessage.getSale());
+            creditInventoryInputPort.credit(saleMessage.value().getSale());
             log.info("End Sale failed payment, compensating transaction: {}", saleMessage);
         }
     }
